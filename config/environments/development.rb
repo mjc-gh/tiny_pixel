2e4680dfb85cdf206d57da98a79ec647696ff06a
Michael Coyne <mjc@hey.com>
2026-04-13 14:05:51 -0400

    Fix loading for middleware in dev and prod envs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 require "active_support/core_ext/integer/time"
-require_relative "../lib/middlewares/silence_request"
+require_relative "../../lib/middlewares/silence_request"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
