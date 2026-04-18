42418f99559fc7fa54463a09b889b53d3b62b889
Michael Coyne <mjc@hey.com>
2026-04-18 07:50:10 -0400

    Add alert and notice flashes

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,8 +1,9 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-import { Slideover } from "tailwindcss-stimulus-components"
+import { Alert, Slideover } from "tailwindcss-stimulus-components"
 
 eagerLoadControllersFrom("controllers", application)
 
 application.register("slideover", Slideover)
+application.register('alert', Alert);
