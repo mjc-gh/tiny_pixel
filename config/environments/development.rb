c1e70d8acd0f1c363ff9066b3d0c556fd0d49293
Michael Coyne <mjc@hey.com>
2026-04-25 11:35:54 -0400

    Update lograge formatter

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,6 +56,9 @@ Rails.application.configure do
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
+  # UUID in logs for
+  config.log_tags = [:uuid]
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
