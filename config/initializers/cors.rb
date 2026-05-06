c9c35136d0eb147f43e2f993fab99776e2a968b6
Michael Coyne <mjc@hey.com>
2026-05-06 13:22:31 +0000

    Remove unused gems

--- config/initializers/cors.rb
+++
@@ -1,9 +0,0 @@
-# config/initializers/cors.rb
-
-#Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  #allow do
-    #origins { _1 }
-
-    #resource "/v1/beacons", headers: :any, methods: [:post], credentials: true
-  #end
-#end
